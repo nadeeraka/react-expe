f60e522fac57cbad54e4df321ffe39f501ea616a
nadeeraka <nimanthanadeeraka@outlook.com>
2018-11-19 20:03:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import about from "./root/about";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Expensify</h1>
+        <div>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={about} />
+            </Switch>
+          </Router>
+        </div>
       </div>
     );
   }
 }
-
+const Home = () => (
+  <div>
+    <p>This is home component</p>
+  </div>
+);
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => (
+  <div>
+    <p>i dont know</p>
+  </div>
+);
+const Add = () => (
+  <div>
+    <p>
+      welcome to add <Link to="/">Go home</Link>
+    </p>
+  </div>
+);
+const Edit = () => (
+  <div>
+    <p>
+      welcome to edit <Link to="/">Go home</Link>
+    </p>
+  </div>
+);
+const help = () => (
+  <div>
+    <p>
+      welcome to help <Link to="/">Go home</Link>
+    </p>
+  </div>
+);
+const notFound = () => (
+  <div>
+    <p>
+      404! <Link to="/">Go home</Link>
+    </p>
+  </div>
+);
+const Header = () => (
+  <div>
+    <h1>Expensify</h1>
+    <Link to="/add">Add</Link>
+    <Link to="/edit">Edit</Link>
+    <Link to="/help">Help</Link>
+  </div>
+);
+
+const rout = (
+  <Router>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/add" component={Add} />
+        <Route path="/edit" component={Edit} />
+        <Route path="/help" component={help} />
+        <Route component={notFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+ReactDOM.render(rout, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
