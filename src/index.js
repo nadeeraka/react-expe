336051522ed24402598d415179729f637fe7efe7
nadeeraka <nimanthanadeeraka@outlook.com>
2018-12-06 16:53:00 +0530

    2nd try v1

--- src/index.js
+++ src/index.js
@@ -1,93 +1,30 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-import {
-  createStore,
-  combineReducers
-} from "redux";
-import uuid from 'uuid';
-// import "./index.css";
-// import App from "./App";
-// import Rout from "./routes/AppRoutes";
+import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
 
-const addExpenses = ({
-  dis = '',
-  note = '',
-  amount = 0,
-  createdAt = 0
-} = {}) => ({
-  type: "ADD_EXPENSE",
-  expense: {
-    id: uuid(),
-    dis,
-    note,
-    amount,
-    createdAt,
-  }
-})
-const removeExpense = ({
-    id
-  } = {}) =>
-  ({
-    type: "REMOVE_EX",
-    id,
-  })
-
-const expense = [];
-
-const expecesReduser = (state = expense, action) => {
+const defaultValues = [];
+const expenceReducer = (state = defaultValues, action) => {
   switch (action.type) {
-    case "ADD_EXPENSE":
-      return [
-        ...state, action.expense
-      ];
-    case "REMOVE_EX":
-      return state.filter(({
-        id
-      }) => id !== action.id);
-
     default:
       return state;
   }
 };
 
-const filter = []
-const filterReduser = (state = filter, action) => {
+const defaultFilter = {
+  sortBy: "date",
+  text: "",
+  startDate: null,
+  endDate: null
+};
+const filterReduser = (state = defaultFilter, action) => {
   switch (action.type) {
     default:
       return state;
   }
-}
+};
 
 const store = createStore(
   combineReducers({
-    expenses: expecesReduser,
+    expence: expenceReducer,
     filter: filterReduser
   })
 );
-let itemOne = store.subscribe(() => {
-  console.log(store.getState());
-});
-let itemTow = store.subscribe(() => {
-  console.log(store.getState());
-});
-
-
-store.dispatch(addExpenses({
-  dis: 'rent',
-  amount: 100
-}))
-console.log(itemOne)
-// store.dispatch(removeExpense({
-//   id: itemOne.expense.id
-// }))
-
-//spread oprator 
-
-let home = {
-  color: 'blue',
-  storys: 'single'
-}
-
-console.log({
-  ...home
-})
