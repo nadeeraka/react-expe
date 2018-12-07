a0c98c38401fd5a4d966e35cd1d53e03e17b3551
nadeeraka <nimanthanadeeraka@outlook.com>
2018-12-07 16:59:05 +0530

    filter1

--- src/playground/filter.js
+++ src/playground/filter.js
@@ -2,16 +2,18 @@
 //coustom filter
 
 let arr = [1, 2, 1, 3, 1, 4, 5, 6];
+let sentence = "hello how are you ";
 
 // let ob = {};
-
 // for (let ob of arr) {
 //   console.log(ob);
 // }
 
 let filter = arr.filter(r => r !== 1);
 let odd = arr.filter(r => r % 2 === 1);
-let even = arr.filter(r => r % 2 === 0);
+let even = arr.filter(r => !(r % 2));
+const word = sentence.split("");
 //console.log(filter);
 //console.log(odd);
-console.log(even);
+//console.log(even);
+console.log(word.filter(r => r.length > 0));
